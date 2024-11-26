34cb5cc5d209d5246620d891a7f3f6b60a41c2a2
difz <muhammadhifzhonharundoyo@mail.ugm.ac.id>
2024-11-26 11:05:49 +0700

    feat: add account routes

--- src/index.js
+++ src/index.js
@@ -44,5 +44,5 @@ app.use("/transaction", require("./routes/transaction"));
 
 //App listen
 app.listen(5000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 5000");
 });
