698b13b9b9f6fc569c925db82b09c13060d8e259
difz <muhammadhifzhonharundoyo@mail.ugm.ac.id>
2024-12-05 23:38:23 +0700

    fix: edit fetch link

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(morgan("dev"));
 
 // CORS
 app.use(cors({
-    origin: ["https://financify.my.id"], // Adjust to your frontend URL
+    origin: ["https://financify.web.id"], // Adjust to your frontend URL
     credentials: true, // Allow credentials (cookies)
   }));
 
